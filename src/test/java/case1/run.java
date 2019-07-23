bd25dacc60283cfdada410e1cd50d4f48363a50c
IshwaryaKS <41283130+IshwaryaKS@users.noreply.github.com>
2019-07-23 12:41:01 +0530

    last commit

--- src/test/java/case1/run.java
+++ src/test/java/case1/run.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features/a7.feature",plugin="json:target\\jsonReporttt.json")
+@CucumberOptions(features="src/test/resources/Features/a7.feature",plugin="json:target\\jsonReporttt.json")
 public class run {
 
 }
